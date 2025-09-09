a74ca45351c9526d9b695d4f4db509b9f6e55964
lovllo <nanmooi31@gmail.com>
2025-09-10 06:24:38 +0700

    Update sadmodnkann.lua

--- sadmodnkann.lua
+++ sadmodnkann.lua
@@ -44,7 +44,7 @@ local function getHRP()
 end
 
 -- Replay function
-local skipPoints = 3
+local skipPoints = 2
 local function playTrack(track)
     if not track or #track < 2 then return end
     local hrp = getHRP()
