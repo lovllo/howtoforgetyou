f734141698b91d4e2c2d63f935d2e9ea58d448d2
lovllo <nanmooi31@gmail.com>
2025-09-10 07:06:25 +0700

    Update nksadflyng.lua

--- nksadflyng.lua
+++ nksadflyng.lua
@@ -57,7 +57,7 @@ local checkpoints = {
 -- ⚡ setting skip
 local skipPoints = 5  -- skip gede buat ngebut
 local safeSkip = 2      -- skip kecil biar CP ga kelewat
-local nearCPDist = 5   -- radius aman (20 studs sekitar CP)
+local nearCPDist = 15  -- radius aman (20 studs sekitar CP)
 
 -- ✅ fungsi cek apakah deket CP
 local function isNearCheckpoint(pos)
